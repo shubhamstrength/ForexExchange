04236a892e2f0d0a8901225297450f9e17d38170
shubhamstrength <shubhamstrength@gmail.com>
2023-01-24 12:42:17 +0530

    base changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ForexExchange/',
+  base: '/',
   plugins: [react()],
   //@ts-ignore
   test: {
